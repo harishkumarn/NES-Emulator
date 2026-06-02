ff99256243767ea8d380e281864fa7cb4230a99d
Harish Kumar Natarajan <harishkr.coc@gmail.com>
2026-06-02 23:08:43 +0530

    Buffer race fixed with - double buffering

--- nes8/src/main/java/com/nes8/graphics/OutputBuffer.java
+++ nes8/src/main/java/com/nes8/graphics/OutputBuffer.java
@@ -15,18 +15,23 @@ public class OutputBuffer {
     Display display;
 
     public Color[][] outputBuffer;
+    Color[][] displayBuffer;
 
 
     public OutputBuffer(Bus bus){
         this.outputBuffer = new Color[DISPLAY_HEIGHT][DISPLAY_WIDTH];
+        this.displayBuffer = new Color[DISPLAY_HEIGHT][DISPLAY_WIDTH];
         this.bus = bus;
     }
 
     public void initDisplay(){
-        this.display = Display.init(DISPLAY_WIDTH, DISPLAY_HEIGHT,Settings.DISPLAY_SCALE, outputBuffer, "NES8");
+        this.display = Display.init(DISPLAY_WIDTH, DISPLAY_HEIGHT,Settings.DISPLAY_SCALE, displayBuffer, "NES8");
     }
 
     public void rerender(){
+        for(int i = 0 ; i < DISPLAY_HEIGHT; i++){
+            System.arraycopy(outputBuffer[i], 0,  displayBuffer[i], 0, DISPLAY_WIDTH);
+        }
         this.display.rerender();
     }
 
